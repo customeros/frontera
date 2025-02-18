761c6abf5018d0d609838f68e6640fd4186e7c25
Alex Calinica <acalinica@customeros.ai>
2025-02-18 08:25:23 +0200

    Remove notification-related environment variables from Dockerfile, GitHub Actions, and TypeScript types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,12 +7,9 @@ interface ImportMetaEnv {
   readonly VITE_TEST_USERNAME: string;
   readonly VITE_CLIENT_APP_URL: string;
   readonly VITE_REALTIME_WS_PATH: string;
-  readonly VITE_NOTIFICATION_URL: string;
   readonly VITE_STRIPE_PUBLIC_KEY: string;
   readonly VITE_MIDDLEWARE_API_URL: string;
   readonly VITE_REALTIME_WS_API_KEY: string;
-  readonly VITE_NOTIFICATION_PROD_APP_IDENTIFIER: string;
-  readonly VITE_NOTIFICATION_TEST_APP_IDENTIFIER: string;
 }
 
 interface ImportMeta {
