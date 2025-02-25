440e942f7cf1a5c6563d90e80d5c79d6fbac4074
mihai@customeros.ai <mihai@customeros.ai>
2025-02-25 10:07:31 +0200

    patch:fix layout for slack config web visitor agent

--- src/routes/agent/components/AgentConfig/Capabilities/SendSlackNotificationCapability/SendSlackNotificationCapability.tsx
+++ src/routes/agent/components/AgentConfig/Capabilities/SendSlackNotificationCapability/SendSlackNotificationCapability.tsx
@@ -76,7 +76,7 @@ export const SendSlackNotificationCapability = observer(() => {
   }
 
   return (
-    <div>
+    <div className='px-4 py-3'>
       <div className='flex items-center justify-between mb-4'>
         <h2 className='text-sm font-medium '>
           {agent.getCapabilityName(
