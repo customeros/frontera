d446ce3b872c96e3ca7f2ad36153a0de95611b55
mihai@customeros.ai <mihai@customeros.ai>
2025-03-20 13:35:04 +0200

    wip

--- src/routes/inbox/components/ThreadsView/components/MailboxList/MailboxList.tsx
+++ src/routes/inbox/components/ThreadsView/components/MailboxList/MailboxList.tsx
@@ -1,37 +1,44 @@
+import { useMemo } from 'react';
+
 import { observer } from 'mobx-react-lite';
+import { MailboxListUsecase } from '@domain/usecases/inbox/mailbox-list.usecase';
 
 import { Icon } from '@ui/media/Icon';
 import { Menu } from '@ui/overlay/Menu/Menu';
 import { Button } from '@ui/form/Button/Button';
 import { useStore } from '@shared/hooks/useStore';
 import { MenuItem, MenuList, MenuButton } from '@ui/overlay/Menu/Menu';
-
 export const MailboxList = observer(() => {
   const store = useStore();
 
   const mailboxList = store.mailboxes.toArray();
+  const usecase = new MailboxListUsecase();
 
   return (
     <div className='flex items-start'>
       <div className='flex flex-col  mb-[-1px] mt-0 flex-1 overflow-visible'>
         <div className='flex flex-col'>
-          <div>
-            <span className='text-grayModern-700 font-semibold text-sm mr-1'>
-              CC:
-            </span>
-          </div>
-          <div>
-            <span className='text-grayModern-700 font-semibold text-sm mr-1'>
-              BCC:
-            </span>
-          </div>
+          {usecase.bccEnabled && (
+            <div>
+              <span className='text-grayModern-700 font-semibold text-sm mr-1'>
+                CC:
+              </span>
+            </div>
+          )}
+          {usecase.bccEnabled && (
+            <div>
+              <span className='text-grayModern-700 font-semibold text-sm mr-1'>
+                BCC:
+              </span>
+            </div>
+          )}
         </div>
         <div>
-          <span className='font-semibold text-sm'>From:</span>
+          <span className='font-semibold text-sm mr-2'>From:</span>
           <Menu>
             <MenuButton asChild>
               <Button
-                size='sm'
+                size='xxs'
                 variant='ghost'
                 className='ml-[-3px]'
                 rightIcon={<Icon name='chevron-down' />}
@@ -47,11 +54,11 @@ export const MailboxList = observer(() => {
           </Menu>
         </div>
       </div>
-      <div className='flex'>
-        <Button size='xxs' variant='ghost'>
+      <div className='flex mb-[-1px] items-center justify-center'>
+        <Button size='xxs' variant='ghost' onClick={usecase.toggleCc}>
           CC
         </Button>
-        <Button size='xxs' variant='ghost'>
+        <Button size='xxs' variant='ghost' onClick={usecase.toggleBcc}>
           BCC
         </Button>
       </div>
