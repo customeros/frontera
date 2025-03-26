1bc2df3e006a368f69157b18c9542d4028fe0016
Alex Calinica <acalinica@customeros.ai>
2025-03-26 08:49:41 +0200

    Remove unused props from IconPicker component

--- src/routes/agent/components/Header/components/IconAndColorPicker.tsx
+++ src/routes/agent/components/Header/components/IconAndColorPicker.tsx
@@ -15,27 +15,10 @@ export const IconAndColorPicker = observer(() => {
 
   return (
     <IconPicker
-      colorsMap={colorMap}
       onIconChange={iconUsecase.execute}
       onColorChange={colorUsecase.execute}
-      iconOptions={iconUsecase.iconOptions}
-      onIconSearch={iconUsecase.setSearchQuery}
-      iconSearchValue={iconUsecase.searchQuery}
       color={colorUsecase.agent?.value.color ?? 'grayModern'}
       icon={(iconUsecase.agent?.value.icon as IconName) ?? 'activity'}
     />
   );
 });
-
-const colorMap = {
-  grayModern: 'bg-grayModern-400 ring-grayModern-400',
-  error: 'bg-error-400 ring-error-400',
-  warning: 'bg-warning-400 ring-warning-400',
-  success: 'bg-success-400 ring-success-400',
-  grayWarm: 'bg-grayWarm-400 ring-grayWarm-400',
-  moss: 'bg-moss-400 ring-moss-400',
-  blueLight: 'bg-blueLight-400 ring-blueLight-400',
-  indigo: 'bg-indigo-400 ring-indigo-400',
-  violet: 'bg-violet-400 ring-violet-400',
-  pink: 'bg-pink-400 ring-pink-400',
-};
