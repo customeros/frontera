e76e841dffde66921008029b6214b602b51dbd1b
mihai@customeros.ai <mihai@customeros.ai>
2025-05-01 09:58:33 +0300

    update timezones query

--- src/infra/repositories/core/settings/queries/getTimezone.generated.ts
+++ src/infra/repositories/core/settings/queries/getTimezone.generated.ts
@@ -4,5 +4,9 @@ export type GetTimezoneQueryVariables = Types.Exact<{ [key: string]: never }>;
 
 export type GetTimezoneQuery = {
   __typename?: 'Query';
-  calendar_timezones: Array<string>;
+  calendar_timezones: Array<{
+    __typename?: 'Timezone';
+    value: string;
+    label: string;
+  }>;
 };
