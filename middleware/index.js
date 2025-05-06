e76658eb65cec8936abb420eccec73283913b823
Alex Calinica <135133139+acalinica@users.noreply.github.com>
2025-05-06 10:52:28 +0300

    patch: refactor tenant logo upload logic (#274)

--- middleware/index.js
+++ middleware/index.js
@@ -307,6 +307,19 @@ async function createServer() {
     followRedirects: true,
   });
 
+  const filesApiProxyForTenant = createProxyMiddleware({
+    pathFilter: '/workspace-logo',
+    pathRewrite: { '^/workspace-logo': '' },
+    target: process.env.CUSTOMER_OS_API_PATH + '/files/v1/workspace-logo',
+    changeOrigin: true,
+    headers: {
+      'X-Openline-API-KEY': process.env.INTERNAL_API_KEY,
+    },
+    logger: console,
+    preserveHeaderKeyCase: true,
+    followRedirects: true,
+  });
+
   const invoiceProxy = createProxyMiddleware({
     pathFilter: '/invoices',
     pathRewrite: { '^/invoices': '' },
@@ -336,6 +349,7 @@ async function createServer() {
   app.use(customerOsApiRestProxyForTenant);
   app.use(internalApiProxy);
   app.use(filesApiProxy);
+  app.use(filesApiProxyForTenant);
   app.use(basConfigProxy);
   app.use(realtimeApiProxy);
   app.use(invoiceProxy);
