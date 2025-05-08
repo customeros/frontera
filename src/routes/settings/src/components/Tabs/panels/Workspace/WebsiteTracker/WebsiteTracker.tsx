ef98466eeedd62a319e4c00c73a2ad6b1c95f2c4
Mihai <163020214+umihai29@users.noreply.github.com>
2025-05-08 17:48:07 +0300

    patch:fix border in website tracker view (#287)

--- src/routes/settings/src/components/Tabs/panels/Workspace/WebsiteTracker/WebsiteTracker.tsx
+++ src/routes/settings/src/components/Tabs/panels/Workspace/WebsiteTracker/WebsiteTracker.tsx
@@ -33,7 +33,7 @@ export const WebsiteTracker = observer(() => {
 
   return (
     <>
-      <div className='px-4 pb-4 flex flex-col  max-w-[448px] text-sm border-r overflow-y-auto max-h-[calc(100vh-1px)]'>
+      <div className='px-4 pb-4 flex flex-col max-w-[448px] h-full text-sm border-r overflow-y-auto max-h-[calc(100vh-1px)]'>
         <div className=' pt-1 flex justify-between items-center sticky top-0 bg-white z-10'>
           <h1 className='font-medium text-base'> Website tracker</h1>
           <Button
