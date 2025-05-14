54caa7c9eab74df002e5b64d78d8f80464f37f3f
Alex Calinica <acalinica@customeros.ai>
2025-05-14 17:49:40 +0300

    patch: Fix incorrect key for ContactsTags filter type

--- src/routes/finder/src/components/Columns/contacts/filterTypes.tsx
+++ src/routes/finder/src/components/Columns/contacts/filterTypes.tsx
@@ -198,7 +198,7 @@ export const getFilterTypes = (store?: RootStore) => {
         <LinkedinOutline className='group-hover:text-grayModern-700grayModernt-grayModern-500 mb-0.5' />
       ),
     },
-    [ColumnViewType.ContactsPersona]: {
+    [ColumnViewType.ContactsTags]: {
       filterType: 'list',
       filterName: 'Tags',
       filterAccesor: ColumnViewType.ContactsPersona,
